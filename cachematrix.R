c1c861998a62541e5f1171587963ca2113662b68
=octavianidevi <=>
2014-04-23 20:24:39 +0700

    Initial code assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a pair of function that cache the inverse of matrix
 
-## Write a short comment describing this function
+
+## This function is used to set the matrix and or set the inverse matrix
+## the user also can get the matrix that has been set
+## and can get the cached inversed matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x 
+        
+        setInverse <- function(invmatrix) m <<- invmatrix
+        
+        getInverse <- function() m 
+        
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function is used to return the inverse matrix 
+## that has been set up in makeCacheMatrix$setInverse()
+## or return the inverse matrix of matrix 'x' 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        # this code is run when the user set setInverse() in makeCacheMatrix function
+        
+        m <- x$getInverse()
+        
+        #checking if the user set the inverse matrix or not
+        if(!is.null(m)) { 
+                message("getting cached data")
+                return(m)
+        }
+        
+        # this code is run if the user haven't setInverse() in makeCacheMatrix function
+        # and give the inversed matrix value as the return
+        
+        data <- x$get()
+        m <- 1/(data[1]*data[4] - data[2]*data[3]) * 
+                matrix(c(data[4],-data[2],-data[3],data[1]),2)
+        
+        x$setInverse(m) #set the inverse matrix
+        m               #return inversed matrix
 }
